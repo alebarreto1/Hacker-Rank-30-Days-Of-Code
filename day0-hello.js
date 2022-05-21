9737afbcb85a0869bd408b4b19b7750b4384b5cc
alebarreto1 <alessandrogongora@hotmail.com>
2022-05-21 17:18:58 -0300

    30 Days Of Code

---
+++ day0-hello.js
@@ -0,0 +1,26 @@
+// Posting here *only* the Submited code on hacker hank's editor screen. In order to make it run on your vs code (or similar) you need to make the necessary implementations
+
+
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.")
+
+    // Write the second line of output that prints the contents of 'inputString' here.
+    console.log(inputString)
+}
+
+
+//-----------------------------------------------------------------------------
+
+//One of the ways to successfully run the code on your vs code:
+
+// let inputString = "Welcome to 30 Days of Code!"
+
+// function processData(inputString) {
+  
+//     console.log("Hello, World.")
+
+//     console.log(inputString)
+// }
+
+// processData(inputString)
