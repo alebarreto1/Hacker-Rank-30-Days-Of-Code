b588a716d3885c6f6f848e35b23e4f73d8425c10
alebarreto1 <alessandrogongora@hotmail.com>
2022-05-22 20:45:56 -0300

    30 Days Of Code

---
+++ day2-operators.js
@@ -0,0 +1,13 @@
+// let meal_cost = 12.00
+// let tip_percent = 20
+// let tax_percent = 8
+
+function solve(meal_cost, tip_percent, tax_percent) {
+    let tip = (meal_cost / 100) * tip_percent;
+    let tax = (tax_percent / 100) * meal_cost;
+    let total_cost = meal_cost + tip + tax;
+    total_cost = Math.round(total_cost);
+    console.log(total_cost);
+}
+
+// solve(meal_cost, tip_percent, tax_percent)
