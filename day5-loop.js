7ea11a86784c697fa400c14ba9bb878ff183f7b7
alebarreto1 <alessandrogongora@hotmail.com>
2022-05-22 20:43:54 -0300

    30 Days Of Code

--- day5-loop.js
+++
@@ -1,7 +0,0 @@
-function main() {
-    const n = parseInt(readLine(), 10);
-    let i;
-    for(i = 1;i <= 10;i++){
-        console.log(`${n} x ${i} = ${n*i}`);
-    }
-}
---
+++ day6-evenOdd.js
@@ -0,0 +1,36 @@
+function processData(input) {
+    //Enter your code here
+    input = input.split("\n");
+
+    for (let i = 1; i < input.length; i++) {
+        let split = input[i].split("");
+
+
+        let even = "";
+        let odd = "";
+
+
+
+        for (let j = 0; j < split.length; j++) {
+            if (j % 2 == 0) {
+                even += split[j];
+            } else {
+                odd += split[j];
+            }
+        }
+
+        console.log(even + " " + odd);
+    }
+
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
